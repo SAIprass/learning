6595a0cdcda2a5ca1c67719d17d855b1442ca8dc
Sai Prasanna <internwl4@waycool.local>
2022-12-05 10:29:36 +0530

    changes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,11 @@
 const authModel = require('../models/authmodel');
-const bcrypt = require('bcryptjs');
-//const salt = bcrypt.genSaltSync(10)
-
+const bcrypt = require('bcryptjs');//The bcrypt npm package is a JavaScript implementation of the bcrypt password hashing function that allows you to easily create a hash out of a password string .
+const salt = bcrypt.genSaltSync(10)
+//salt npm = A salt is a random piece of data that is used as an additional input to a one-way function that hashes data or a password. 
 module.exports = {
     register:async(req,res,next) => {
-        const checkemail = await authModel.findOne({email:req.body.email});
-        if(checkemail){
+        const checkemail = await authModel.findOne({email:req.body.email});//checking if the email exists or not
+        if(checkemail){//if exists it should display the below message
             return res.status(400).send({status:false,message:"email already exists"});
         }
         const newregister = new authModel({
@@ -18,5 +18,23 @@ module.exports = {
             return res.status(400).json({status:false,message:"failed to register"});
         }
             return res.status(200).json({status:true,message:"user registered successfully",registerdata});
+    },
+    signin:async(req,res,next) => {
+        const checkdetails = await authModel.findOne({email:req.body.email},{password:req.body.password});
+        if(!checkdetails){
+            console.log("data",checkdetails)
+            return res.status(400).send({status:false,message:"invalid crendentials! please check your crendtials"});
+
+        }
+        else{
+            console.log("datain",checkdetails)
+            return res.status(200).send({status:true,message:"login successful"})
+        }
+        
+
+    },
+    signout:async(req,res,next)=>{
+        req.session=null;
+        return res.status(200).send({status:true,message:"logout successful"})
     }
 }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authcontroller = require('../controllers/auth.controller');
 router.post("/register",authcontroller.register);
+router.post("/signin",authcontroller.signin);
+router.get("/signout",authcontroller.signout)
 module.exports = router
